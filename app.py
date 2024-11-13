aaf3d72ecd512537ae44ec6d329a050d6c427a87
Emmynaess <143788722+Emmynaess@users.noreply.github.com>
2024-11-13 21:08:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import requests
+from datetime import datetime
+import time
+
+url = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.0686/lat/59.3293/data.json"
+
 def fetch_weather_data():
     response = requests.get(url)
     if response.status_code == 200:
@@ -29,26 +35,8 @@ def fetch_weather_data():
     else:
         return []
 
-
-"""from flask import Flask, jsonify
-import requests
-
-app = Flask(__name__)
-
-@app.route('/temperature')
-def get_temperature():
-    api_url = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.0686/lat/59.3293/data.json"
-    
-    try:
-        response = requests.get(api_url)
-        data = response.json()
-        
-        temperature_data = data['timeSeries'][0]['parameters']
-        temp_value = next(item['values'][0] for item in temperature_data if item["name"] == "t")
-
-        return jsonify({"temperature": temp_value})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)"""
+    while True:
+        weather_data = fetch_weather_data()
+        print(weather_data)
+        time.sleep(3600)
