b5b7ced0269decf59efc0bcc6edd1cbcbaa58c99
Emmynaess <143788722+Emmynaess@users.noreply.github.com>
2024-11-13 22:17:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,18 @@
-from flask import Flask, jsonify
 import requests
-from datetime import datetime
+import os
 
-app = Flask(__name__)
-
-url = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.0686/lat/59.3293/data.json"
-
-def fetch_weather_data():
-    response = requests.get(url)
+def fetch_weather():
+    api_url = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.0686/lat/59.3293/data.json"
+    headers = {
+        "Accept": "application/json",
+        "User-Agent": "weather-app"
+    }
+    response = requests.get(api_url, headers=headers)
     if response.status_code == 200:
-        data = response.json()
-        weather_data = []
-        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        for time_series in data['timeSeries']:
-            timestamp_str = time_series['validTime'].replace('T', ' ').replace('Z', '')
-            timestamp = datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S')
-            time_diff_hours = (timestamp - datetime.now()).total_seconds() / 3600
-
-            if 0 <= time_diff_hours <= 24:
-                datum = timestamp_str.split()[0]
-                hour = int(timestamp_str.split()[1][:2])
-                precipitation = next(param['values'][0] for param in time_series['parameters'] if param['name'] == 'pcat')
-                temperature = next(param['values'][0] for param in time_series['parameters'] if param['name'] == 't')
-                provider = 'SMHI'
-                precipitation = "Yes" if precipitation > 0 else "No"
-
-                weather_data.append({
-                    'Created': current_time,
-                    'Datum': datum,
-                    'Hour': hour,
-                    'Precipitation': precipitation,
-                    'Temperature': temperature,
-                    'Provider': provider,
-                })
-        return weather_data
+        return response.json()
     else:
-        return []
+        raise Exception("Failed to fetch data from SMHI API")
 
-@app.route('/weather')
-def get_weather():
-    weather_data = fetch_weather_data()
-    return jsonify(weather_data)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    weather_data = fetch_weather()
+    print("Weather data:", weather_data)
