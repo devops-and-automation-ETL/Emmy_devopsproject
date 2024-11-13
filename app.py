8f21b10128b7c5246b610932ba55fc3fa21abbe8
Emmynaess <143788722+Emmynaess@users.noreply.github.com>
2024-11-13 12:08:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,36 @@
-from flask import Flask, jsonify
+def fetch_weather_data():
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        weather_data = []
+        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        for time_series in data['timeSeries']:
+            timestamp_str = time_series['validTime'].replace('T', ' ').replace('Z', '')
+            timestamp = datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S')
+            time_diff_hours = (timestamp - datetime.now()).total_seconds() / 3600
+
+            if 0 <= time_diff_hours <= 24:
+                datum = timestamp_str.split()[0]
+                hour = int(timestamp_str.split()[1][:2])
+                precipitation = next(param['values'][0] for param in time_series['parameters'] if param['name'] == 'pcat')
+                temperature = next(param['values'][0] for param in time_series['parameters'] if param['name'] == 't')
+                provider = 'SMHI'
+                precipitation = "Yes" if precipitation > 0 else "No"
+
+                weather_data.append({
+                    'Created': current_time,
+                    'Datum': datum,
+                    'Hour': hour,
+                    'Precipitation': precipitation,
+                    'Temperature': temperature,
+                    'Provider': provider,
+                })
+        return weather_data
+    else:
+        return []
+
+
+"""from flask import Flask, jsonify
 import requests
 
 app = Flask(__name__)
@@ -19,4 +51,4 @@ def get_temperature():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)"""
