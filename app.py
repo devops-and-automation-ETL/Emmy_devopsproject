372344ca647c192da022bff51fce1a2a53755b7d
Emmynaess <143788722+Emmynaess@users.noreply.github.com>
2024-11-14 11:31:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
 import requests
-import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def fetch_weather():
-    # API URL för att hämta senaste temperaturen
     api_url = "https://opendata-download-metobs.smhi.se/api/version/latest/parameter/1/station/159880/period/latest-hour/data.json"
     headers = {
         "Accept": "application/json",
@@ -15,11 +13,10 @@ def fetch_weather():
     response = requests.get(api_url, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        # Extrahera temperaturvärdet
         temperature = data["value"][0]["value"]
-        return jsonify({"temperature": f"{temperature}°C"})
+        return f"Current temperature: {temperature}°C"
     else:
-        return jsonify({"error": "Failed to fetch data from SMHI API"}), response.status_code
+        return "Failed to fetch data from SMHI API", response.status_code
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
